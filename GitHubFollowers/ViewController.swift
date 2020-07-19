10d97a0290aeea24ee86d74c6719b2de8c5782e2
Leandro Estrada <leandro.estrada@cwi.com.br>
2020-07-19 11:52:31 -0300

    Starting the project

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Leandro de Araujo Estrada on 13/07/20.
+//  Copyright © 2020 Leandro de Araujo Estrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
